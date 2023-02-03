518b25e9800628082c521773237d531d8e973c54
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 15:09:12 -0300

    feat: add validator error messages

---
+++ src/validators/errorMessages.ts
@@ -0,0 +1,11 @@
+export const messages = {
+  required: 'required field',
+  empty: 'field cannot be empty',
+  length: 'invalid length',
+  format: 'invalid format',
+  email: 'invalid email format',
+  lowercase: 'field must be in lower case',
+  stringValue: 'field value must be a string',
+  intValue: 'field value must be a integer',
+  onlyBoolean: 'field value must be a boolean. Boolean(true:false)',
+};
