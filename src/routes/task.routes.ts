a52468e8f7e593d80db81ad55f05cf51fcbf22f9
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 12:31:05 -0300

    feat: add addMember and removeMember routes

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -7,4 +7,6 @@ taskRouter.post('/', taskController.create);
 taskRouter.get('/:id', taskController.read);
 taskRouter.get('/', taskController.readAll);
 taskRouter.put('/update/:id', taskController.update);
+taskRouter.put('/addmember/:id', taskController.addMember);
+taskRouter.put('/removemember/:id', taskController.removeMember);
 taskRouter.delete('/delete/:id', taskController.delete);
