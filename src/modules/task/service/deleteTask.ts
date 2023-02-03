076532934cf310b883ef51df0c6df0e6c8fb90c1
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 13:36:46 -0300

    refactor: modify delete task service

--- src/modules/task/service/deleteTask.ts
+++ src/modules/task/service/deleteTask.ts
@@ -1,12 +1,28 @@
+import { Member, Task } from '@prisma/client';
 import { HttpException } from '../../../errors/HttpException';
+import { removeItem } from '../../../helper/removeItem';
+import { IMember } from '../../../types/IMember';
+import { memberRepository } from '../../member/repository/memberRepository';
 import { taskRepository } from '../repository/taskRepository';
 
-export const deleteTask = async (id: string) => {
-  const task = await taskRepository.read(id);
+export const deleteTask = async (task_id: string) => {
+  const task = await taskRepository.read(task_id);
 
   if (!task) {
     throw new HttpException(404, 'Task not found');
   }
+  
+  const { member } = <Task>task;
 
-  return await taskRepository.delete(id);
+  const membersTask = <IMember[]>member;
+
+  membersTask.map(async member => {
+    const getMember = await memberRepository.readByEmail(member.email);
+    const { id } = <Member>getMember;
+    const memberTask = <string[]>getMember?.task;
+    const newArrayMembers = removeItem(memberTask, task_id);
+    await memberRepository.removeTask(id, newArrayMembers);
+  });
+
+  return await taskRepository.delete(task_id);
 };
