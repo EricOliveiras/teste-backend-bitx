37ce5584f6a76fea1f5a06820a36c8f7c7494a99
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 15:20:35 -0300

    feat: add validators in member routes

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -1,10 +1,34 @@
 import { Router } from 'express';
 import { memberController } from '../modules/member/controller/memberController';
+import { handleValidator } from '../validators/handleValidator';
+import { memberValidator } from '../validators/member';
 
 export const memberRouter = Router();
 
-memberRouter.post('/', memberController.create);
-memberRouter.get('/:id', memberController.read);
-memberRouter.get('/', memberController.readAll);
-memberRouter.put('/update/:id', memberController.update);
-memberRouter.delete('/delete/:id', memberController.delete);
+memberRouter.post('/',
+  memberValidator.create,
+  handleValidator,
+  memberController.create
+);
+
+memberRouter.get('/search/:id',
+  memberValidator.id,
+  handleValidator,
+  memberController.read
+);
+
+memberRouter.get('/',
+  memberController.readAll
+);
+
+memberRouter.put('/update/:id',
+  memberValidator.create,
+  handleValidator,
+  memberController.update
+);
+
+memberRouter.delete('/delete/:id',
+  memberValidator.id,
+  handleValidator,
+  memberController.delete
+);
