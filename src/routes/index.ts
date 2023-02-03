23ea7ab9ff23bb2b0338bd06f775873efd026a34
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 14:29:20 -0300

    feat: import member routes in index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { memberRouter } from './member.routes';
 import { rootRouter } from './root.routes';
 import { taskRouter } from './task.routes';
 
 export const routes = Router();
 
 routes.use('/task', taskRouter);
+routes.use('/member', memberRouter);
 routes.use('/', rootRouter);
