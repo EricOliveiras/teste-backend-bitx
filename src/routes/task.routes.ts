eb6264fb1817dd60a552694cd1828f25f5258381
Eric Oliveira <oiericoi@hotmail.com>
2023-02-03 15:14:33 -0300

    feat: add validator in task routes

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,12 +1,49 @@
 import { Router } from 'express';
 import { taskController } from '../modules/task/controller/taskController';
+import { handleValidator } from '../validators/handleValidator';
+import { taskValidator } from '../validators/task';
 
 export const taskRouter = Router();
 
-taskRouter.post('/', taskController.create);
-taskRouter.get('/:id', taskController.read);
-taskRouter.get('/', taskController.readAll);
-taskRouter.put('/update/:id', taskController.update);
-taskRouter.put('/addmember/:id', taskController.addMember);
-taskRouter.put('/removemember/:id', taskController.removeMember);
-taskRouter.delete('/delete/:id', taskController.delete);
+taskRouter.post('/',
+  taskValidator.create,
+  handleValidator,
+  taskController.create
+);
+
+taskRouter.get('/search/:id',
+  taskValidator.id,
+  handleValidator,
+  taskController.read
+);
+
+taskRouter.get('/',
+  taskController.readAll
+);
+
+taskRouter.put('/update/:id',
+  taskValidator.id,
+  taskValidator.update,
+  handleValidator,
+  taskController.update
+);
+
+taskRouter.put('/addmember/:id',
+  taskValidator.id,
+  taskValidator.addMember,
+  handleValidator,
+  taskController.addMember
+);
+
+taskRouter.put('/removemember/:id',
+  taskValidator.id,
+  taskValidator.removeMember,
+  handleValidator,
+  taskController.removeMember
+);
+
+taskRouter.delete('/delete/:id',
+  taskValidator.id,
+  handleValidator,
+  taskController.delete
+);
